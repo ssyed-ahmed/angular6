e1878605e01dd89fff58c9041ee616c237b4f51e
unknown <sumair_ahmed4@yahoo.co.in>
2018-09-01 12:37:02 +1000

    Updated routing file

--- crmapp/src/app/app-routing/app-routing.module.ts
+++ crmapp/src/app/app-routing/app-routing.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AccountListComponentComponent } from 'src/app/account-list-component/account-list-component.component';
+import { AccountListComponent } from 'src/app/account-list/account-list.component';
+import { AccountCreateComponent } from 'src/app/account-create/account-create.component';
+import { ContactListComponent } from 'src/app/contact-list/contact-list.component';
+import { ContactCreateComponent } from 'src/app/contact-create/contact-create.component';
+import { LeadListComponent } from 'src/app/lead-list/lead-list.component';
+import { combineLatest } from 'rxjs/internal/observable/combineLatest';
+import { Component } from '@angular/core/src/metadata/directives';
+import { LeadCreateComponent } from 'src/app/lead-create/lead-create.component';
+import { OpportunityListComponent } from 'src/app/opportunity-list/opportunity-list.component';
+import { OpportunityCreateComponent } from 'src/app/opportunity-create/opportunity-create.component';
 
 const routes:Routes = [
   {
@@ -10,8 +19,37 @@ const routes:Routes = [
   },
   {
     path: 'accounts',
-    component: AccountListComponentComponent
+    component: AccountListComponent
+  },
+  {
+    path: 'create-account',
+    component: AccountCreateComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactListComponent
+  },
+  {
+    path: 'create-contact',
+    component: ContactCreateComponent
+  },
+  {
+    path: 'leads',
+    component: LeadListComponent
+  },
+  {
+    path: 'create-lead',
+    component: LeadCreateComponent
+  },
+  {
+    path: 'opportunities',
+    component: OpportunityListComponent
+  },
+  {
+    path: 'create-opportunity',
+    component: OpportunityCreateComponent
   }
+
 ];
 
 @NgModule({
