88ee6870613a3ec038eb568f3f2655b650fdd32f
unknown <sumair_ahmed4@yahoo.co.in>
2018-09-05 22:28:15 +1000

    Added list of mock heroes

--- angular-heroes/src/app/heroes/heroes.component.ts
+++ angular-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +9,15 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
