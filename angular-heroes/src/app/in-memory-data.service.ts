aa003e8531bf772877d9b5c6e8a1f533a1bf499a
unknown <sumair_ahmed4@yahoo.co.in>
2018-09-12 00:08:27 +1000

    Added more heroes

--- angular-heroes/src/app/in-memory-data.service.ts
+++ angular-heroes/src/app/in-memory-data.service.ts
@@ -17,7 +17,8 @@ export class InMemoryDataService implements InMemoryDbService {
       new Hero(14, 'Ironman'),
       new Hero(15, 'Spiderman'),
       new Hero(16, 'Superman'),
-      new Hero(17, 'Wonderwoman')
+      new Hero(17, 'Wonderwoman'),
+      new Hero(18, 'Greenlantern')
     ];
 
     return {heroes};
--- angular-heroes/src/app/mock-heroes.ts
+++ angular-heroes/src/app/mock-heroes.ts
@@ -7,5 +7,6 @@ export const HEROES: Hero[] = [
   new Hero(14, 'Ironman'),
   new Hero(15, 'Spiderman'),
   new Hero(16, 'Superman'),
-  new Hero(17, 'Wonderwoman')
+  new Hero(17, 'Wonderwoman'),
+  new Hero(18, 'Greenlantern')
 ];
