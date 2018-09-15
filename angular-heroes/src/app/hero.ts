7115787510a000ec09abb4d4e8d88479d51ac92f
unknown <sumair_ahmed4@yahoo.co.in>
2018-09-15 10:43:44 +1000

    Added updateHero method and more properties for hero class

--- angular-heroes/src/app/hero.ts
+++ angular-heroes/src/app/hero.ts
@@ -1,9 +1,13 @@
 export class Hero {
     id: number;
     name: string;
+    icon: string;
+    description: string;
 
-    constructor(id: number, name: string) {
+    constructor(id: number, name: string, icon: string, description: string) {
         this.id = id;
         this.name = name;
+        this.icon = icon;
+        this.description = description;
     }
 }
--- angular-heroes/src/app/mock-heroes.ts
+++ angular-heroes/src/app/mock-heroes.ts
@@ -1,12 +1,12 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  new Hero(11, 'Antman'),
-  new Hero(12, 'Batman'),
-  new Hero(13, 'Hulk'),
-  new Hero(14, 'Ironman'),
-  new Hero(15, 'Spiderman'),
-  new Hero(16, 'Superman'),
-  new Hero(17, 'Wonderwoman'),
-  new Hero(18, 'Greenlantern')
+    new Hero(11, 'Ant man', 'antman', 'This is ant man.'),
+    new Hero(12, 'Bat man', 'batman', 'This is bat man.'),
+    new Hero(13, 'Hulk', 'hulk', 'This is hulk.'),
+    new Hero(14, 'Iron man', 'ironman', 'This is iron man.'),
+    new Hero(15, 'Spider man', 'spiderman', 'This is spider man.'),
+    new Hero(16, 'Super man', 'superman', 'This is super man.'),
+    new Hero(17, 'Wonder woman', 'wonderwoman', 'This is wonder woman.'),
+    new Hero(18, 'Green lantern', 'greenlantern', 'This is green lantern.')
 ];
