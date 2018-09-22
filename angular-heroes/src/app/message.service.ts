f00e34d5f745d9d2c0e4300544c8db5df038755a
unknown <sumair_ahmed4@yahoo.co.in>
2018-09-22 11:15:44 +1000

    Used ngSwitch to display icons for different types of messages

--- angular-heroes/src/app/message.service.ts
+++ angular-heroes/src/app/message.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Message } from './message';
+import { Message, Severity } from './message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  messages: Message[] = [];
+  messages: Message[] = this.getDummyMessages();
 
   constructor() { }
 
@@ -16,5 +16,21 @@ export class MessageService {
 
   clear(): void {
     this.messages = [];
+  }
+
+  getDummyMessages(): Message[] {
+    let messages = [];
+    let msg = new Message('This is an info message', Severity.INFO, new Date(), null);
+    messages.push(msg);
+    msg = new Message('This is a warning message', Severity.WARNING, new Date(), null);
+    messages.push(msg);
+    msg = new Message('This is a debug message', Severity.DEBUG, new Date(), null);
+    messages.push(msg);
+    msg = new Message('This is an error message', Severity.ERROR, new Date(), null);
+    messages.push(msg);
+    msg = new Message('This is a fatal message', Severity.FATAL, new Date(), null);
+    messages.push(msg);
+
+    return messages;
   }
 }
