73337b005000521ba43a7c42c52ad33268714e2a
unknown <sumair_ahmed4@yahoo.co.in>
2018-10-05 20:56:00 +1000

    Updated form labels to show required fields and attempted to use child routes

--- angular-heroes/src/app/app-routing.module.ts
+++ angular-heroes/src/app/app-routing.module.ts
@@ -6,11 +6,21 @@ import { HeroDetailComponent } from 'src/app/hero-detail/hero-detail.component';
 import { LogsComponent } from 'src/app/logs/logs.component';
 import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
 import { StatisticsComponent } from 'src/app/statistics/statistics.component';
+import { HeroDetailGeneralComponent } from 'src/app/hero-detail-general/hero-detail-general.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'heroes/:id/:fromState', component: HeroDetailComponent },
+  // { path: 'heroes/:id/:fromState', component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    children: [
+      {
+        path: ':id/:fromState',
+        component: HeroDetailComponent
+      }
+    ]
+  },
   { path: 'logs', component: LogsComponent },
   { path: 'statistics', component: StatisticsComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
