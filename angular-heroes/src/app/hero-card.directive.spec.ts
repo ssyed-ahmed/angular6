92ad507551cc155650cc2af4e2a398810ea36b5c
unknown <sumair_ahmed4@yahoo.co.in>
2018-10-12 10:59:34 +1100

    Added hero-card directive

---
+++ angular-heroes/src/app/hero-card.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HeroCardDirective } from './hero-card.directive';
+
+describe('HeroCardDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HeroCardDirective();
+    expect(directive).toBeTruthy();
+  });
+});
